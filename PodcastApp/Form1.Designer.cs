69928a70c95bed21429f45e87ff91e217f5eeebb
Carolin Nygren <nygren.c@gmail.com>
2020-10-22 11:45:08 +0200

    1

---
+++ PodcastApp/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace PodcastApp
+{
+    partial class Podcast_app
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblUppdateringsf = new System.Windows.Forms.Label();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.cmbxFrekvens = new System.Windows.Forms.ComboBox();
+            this.cmbxKategori = new System.Windows.Forms.ComboBox();
+            this.btnNy = new System.Windows.Forms.Button();
+            this.btnSpara = new System.Windows.Forms.Button();
+            this.btnTaBort = new System.Windows.Forms.Button();
+            this.lblAvsnitt = new System.Windows.Forms.Label();
+            this.lstAvsnitt = new System.Windows.Forms.ListBox();
+            this.lblKategorier = new System.Windows.Forms.Label();
+            this.lstKategorier = new System.Windows.Forms.ListBox();
+            this.txtValdKategori = new System.Windows.Forms.TextBox();
+            this.btnNyKategori = new System.Windows.Forms.Button();
+            this.btnSparaKategori = new System.Windows.Forms.Button();
+            this.btnTaBortKategori = new System.Windows.Forms.Button();
+            this.lblRubrikPodcastInfo = new System.Windows.Forms.Label();
+            this.lblPodcastBeskrivning = new System.Windows.Forms.Label();
+            this.podcastInfoGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.podcastInfoGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(26, 313);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(35, 13);
+            this.lblURL.TabIndex = 1;
+            this.lblURL.Text = "label1";
+            // 
+            // lblUppdateringsf
+            // 
+            this.lblUppdateringsf.AutoSize = true;
+            this.lblUppdateringsf.Location = new System.Drawing.Point(291, 313);
+            this.lblUppdateringsf.Name = "lblUppdateringsf";
+            this.lblUppdateringsf.Size = new System.Drawing.Size(35, 13);
+            this.lblUppdateringsf.TabIndex = 2;
+            this.lblUppdateringsf.Text = "label2";
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(551, 312);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(35, 13);
+            this.lblKategori.TabIndex = 3;
+            this.lblKategori.Text = "label3";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(29, 339);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(255, 20);
+            this.txtURL.TabIndex = 4;
+            // 
+            // cmbxFrekvens
+            // 
+            this.cmbxFrekvens.FormattingEnabled = true;
+            this.cmbxFrekvens.Location = new System.Drawing.Point(294, 337);
+            this.cmbxFrekvens.Name = "cmbxFrekvens";
+            this.cmbxFrekvens.Size = new System.Drawing.Size(246, 21);
+            this.cmbxFrekvens.TabIndex = 5;
+            // 
+            // cmbxKategori
+            // 
+            this.cmbxKategori.FormattingEnabled = true;
+            this.cmbxKategori.Location = new System.Drawing.Point(554, 337);
+            this.cmbxKategori.Name = "cmbxKategori";
+            this.cmbxKategori.Size = new System.Drawing.Size(186, 21);
+            this.cmbxKategori.TabIndex = 6;
+            // 
+            // btnNy
+            // 
+            this.btnNy.Location = new System.Drawing.Point(414, 390);
+            this.btnNy.Name = "btnNy";
+            this.btnNy.Size = new System.Drawing.Size(75, 23);
+            this.btnNy.TabIndex = 7;
+            this.btnNy.Text = "button1";
+            this.btnNy.UseVisualStyleBackColor = true;
+            // 
+            // btnSpara
+            // 
+            this.btnSpara.Location = new System.Drawing.Point(538, 390);
+            this.btnSpara.Name = "btnSpara";
+            this.btnSpara.Size = new System.Drawing.Size(75, 23);
+            this.btnSpara.TabIndex = 8;
+            this.btnSpara.Text = "button2";
+            this.btnSpara.UseVisualStyleBackColor = true;
+            // 
+            // btnTaBort
+            // 
+            this.btnTaBort.Location = new System.Drawing.Point(665, 390);
+            this.btnTaBort.Name = "btnTaBort";
+            this.btnTaBort.Size = new System.Drawing.Size(75, 23);
+            this.btnTaBort.TabIndex = 9;
+            this.btnTaBort.Text = "button3";
+            this.btnTaBort.UseVisualStyleBackColor = true;
+            // 
+            // lblAvsnitt
+            // 
+            this.lblAvsnitt.AutoSize = true;
+            this.lblAvsnitt.Location = new System.Drawing.Point(29, 427);
+            this.lblAvsnitt.Name = "lblAvsnitt";
+            this.lblAvsnitt.Size = new System.Drawing.Size(35, 13);
+            this.lblAvsnitt.TabIndex = 10;
+            this.lblAvsnitt.Text = "label4";
+            // 
+            // lstAvsnitt
+            // 
+            this.lstAvsnitt.FormattingEnabled = true;
+            this.lstAvsnitt.Location = new System.Drawing.Point(26, 458);
+            this.lstAvsnitt.Name = "lstAvsnitt";
+            this.lstAvsnitt.Size = new System.Drawing.Size(714, 290);
+            this.lstAvsnitt.TabIndex = 12;
+            // 
+            // lblKategorier
+            // 
+            this.lblKategorier.AutoSize = true;
+            this.lblKategorier.Location = new System.Drawing.Point(779, 23);
+            this.lblKategorier.Name = "lblKategorier";
+            this.lblKategorier.Size = new System.Drawing.Size(35, 13);
+            this.lblKategorier.TabIndex = 13;
+            this.lblKategorier.Text = "label5";
+            // 
+            // lstKategorier
+            // 
+            this.lstKategorier.FormattingEnabled = true;
+            this.lstKategorier.Location = new System.Drawing.Point(782, 43);
+            this.lstKategorier.Name = "lstKategorier";
+            this.lstKategorier.Size = new System.Drawing.Size(389, 251);
+            this.lstKategorier.TabIndex = 14;
+            // 
+            // txtValdKategori
+            // 
+            this.txtValdKategori.Location = new System.Drawing.Point(782, 337);
+            this.txtValdKategori.Name = "txtValdKategori";
+            this.txtValdKategori.Size = new System.Drawing.Size(389, 20);
+            this.txtValdKategori.TabIndex = 15;
+            // 
+            // btnNyKategori
+            // 
+            this.btnNyKategori.Location = new System.Drawing.Point(782, 390);
+            this.btnNyKategori.Name = "btnNyKategori";
+            this.btnNyKategori.Size = new System.Drawing.Size(113, 23);
+            this.btnNyKategori.TabIndex = 16;
+            this.btnNyKategori.Text = "button4";
+            this.btnNyKategori.UseVisualStyleBackColor = true;
+            // 
+            // btnSparaKategori
+            // 
+            this.btnSparaKategori.Location = new System.Drawing.Point(928, 390);
+            this.btnSparaKategori.Name = "btnSparaKategori";
+            this.btnSparaKategori.Size = new System.Drawing.Size(113, 23);
+            this.btnSparaKategori.TabIndex = 17;
+            this.btnSparaKategori.Text = "button5";
+            this.btnSparaKategori.UseVisualStyleBackColor = true;
+            // 
+            // btnTaBortKategori
+            // 
+            this.btnTaBortKategori.Location = new System.Drawing.Point(1058, 390);
+            this.btnTaBortKategori.Name = "btnTaBortKategori";
+            this.btnTaBortKategori.Size = new System.Drawing.Size(113, 23);
+            this.btnTaBortKategori.TabIndex = 18;
+            this.btnTaBortKategori.Text = "button6";
+            this.btnTaBortKategori.UseVisualStyleBackColor = true;
+            // 
+            // lblRubrikPodcastInfo
+            // 
+            this.lblRubrikPodcastInfo.AutoSize = true;
+            this.lblRubrikPodcastInfo.Location = new System.Drawing.Point(782, 458);
+            this.lblRubrikPodcastInfo.Name = "lblRubrikPodcastInfo";
+            this.lblRubrikPodcastInfo.Size = new System.Drawing.Size(35, 13);
+            this.lblRubrikPodcastInfo.TabIndex = 19;
+            this.lblRubrikPodcastInfo.Text = "label6";
+            // 
+            // lblPodcastBeskrivning
+            // 
+            this.lblPodcastBeskrivning.AutoSize = true;
+            this.lblPodcastBeskrivning.Location = new System.Drawing.Point(785, 485);
+            this.lblPodcastBeskrivning.Name = "lblPodcastBeskrivning";
+            this.lblPodcastBeskrivning.Size = new System.Drawing.Size(35, 13);
+            this.lblPodcastBeskrivning.TabIndex = 20;
+            this.lblPodcastBeskrivning.Text = "label7";
+            // 
+            // podcastInfoGridView
+            // 
+            this.podcastInfoGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.podcastInfoGridView.Location = new System.Drawing.Point(29, 23);
+            this.podcastInfoGridView.Name = "podcastInfoGridView";
+            this.podcastInfoGridView.Size = new System.Drawing.Size(711, 271);
+            this.podcastInfoGridView.TabIndex = 21;
+            // 
+            // Podcast_app
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 776);
+            this.Controls.Add(this.podcastInfoGridView);
+            this.Controls.Add(this.lblPodcastBeskrivning);
+            this.Controls.Add(this.lblRubrikPodcastInfo);
+            this.Controls.Add(this.btnTaBortKategori);
+            this.Controls.Add(this.btnSparaKategori);
+            this.Controls.Add(this.btnNyKategori);
+            this.Controls.Add(this.txtValdKategori);
+            this.Controls.Add(this.lstKategorier);
+            this.Controls.Add(this.lblKategorier);
+            this.Controls.Add(this.lstAvsnitt);
+            this.Controls.Add(this.lblAvsnitt);
+            this.Controls.Add(this.btnTaBort);
+            this.Controls.Add(this.btnSpara);
+            this.Controls.Add(this.btnNy);
+            this.Controls.Add(this.cmbxKategori);
+            this.Controls.Add(this.cmbxFrekvens);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.lblKategori);
+            this.Controls.Add(this.lblUppdateringsf);
+            this.Controls.Add(this.lblURL);
+            this.Name = "Podcast_app";
+            this.Text = "Podcast app";
+            ((System.ComponentModel.ISupportInitialize)(this.podcastInfoGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblUppdateringsf;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.ComboBox cmbxFrekvens;
+        private System.Windows.Forms.ComboBox cmbxKategori;
+        private System.Windows.Forms.Button btnNy;
+        private System.Windows.Forms.Button btnSpara;
+        private System.Windows.Forms.Button btnTaBort;
+        private System.Windows.Forms.Label lblAvsnitt;
+        private System.Windows.Forms.ListBox lstAvsnitt;
+        private System.Windows.Forms.Label lblKategorier;
+        private System.Windows.Forms.ListBox lstKategorier;
+        private System.Windows.Forms.TextBox txtValdKategori;
+        private System.Windows.Forms.Button btnNyKategori;
+        private System.Windows.Forms.Button btnSparaKategori;
+        private System.Windows.Forms.Button btnTaBortKategori;
+        private System.Windows.Forms.Label lblRubrikPodcastInfo;
+        private System.Windows.Forms.Label lblPodcastBeskrivning;
+        private System.Windows.Forms.DataGridView podcastInfoGridView;
+    }
+}
+
