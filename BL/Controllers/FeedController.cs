f15c40b429d1715eab8aaa2b07f6a3a243ede0b2
oskar <oskar@DESKTOP-MVT4NP3>
2020-11-02 10:33:55 +0100

    namnförtydligande

--- BL/Controllers/FeedController.cs
+++ BL/Controllers/FeedController.cs
@@ -13,7 +13,7 @@ namespace BL.Controllers
     public class FeedController
     {
         private readonly RSSReader reader = new RSSReader();
-        private readonly FeedRepository feed = new FeedRepository();
+        private readonly FeedRepository feedRepository = new FeedRepository();
 
         public FeedController()
         {
@@ -28,7 +28,7 @@ namespace BL.Controllers
                 pod.Namn = namn;
                 pod.Kategorier = kategori;
                 pod.UppdateringsTid = frekvens;
-                feed.Create(pod);
+                feedRepository.Create(pod);
             }
             catch (Exception e)
             {
@@ -41,32 +41,32 @@ namespace BL.Controllers
 
         public Feed GetFeed(string url)
         {
-            return feed.GetAll().Where(f => string.Equals(f.Url, url, StringComparison.OrdinalIgnoreCase)).First();
+            return feedRepository.GetAll().Where(f => string.Equals(f.Url, url, StringComparison.OrdinalIgnoreCase)).First();
         }
 
         public List<Feed> GetAll()
         {
-            return feed.GetAll();
+            return feedRepository.GetAll();
         }
 
         public List<Feed> GetAllKategori(string kategori)
         {
-            return (feed.GetAll().Where(f => string.Equals(f.Kategorier, kategori, StringComparison.OrdinalIgnoreCase))).ToList();
+            return (feedRepository.GetAll().Where(f => string.Equals(f.Kategorier, kategori, StringComparison.OrdinalIgnoreCase))).ToList();
         }
 
         public void ChangeKategori(string kategori, string old)
         {
-            feed.ChangeKategori(kategori, old);
+            feedRepository.ChangeKategori(kategori, old);
         }
 
         public void DeleteByKategori(string kategori)
         {
 
             List<Feed> feedDelete = new List<Feed>();
-            for (int i = 0; i < feed.GetAll().Count; i++)
+            for (int i = 0; i < feedRepository.GetAll().Count; i++)
             {
-                if (string.Equals(feed.GetAll()[i].Kategorier, kategori, StringComparison.OrdinalIgnoreCase))
-                    feedDelete.Add(feed.GetAll()[i]);
+                if (string.Equals(feedRepository.GetAll()[i].Kategorier, kategori, StringComparison.OrdinalIgnoreCase))
+                    feedDelete.Add(feedRepository.GetAll()[i]);
             }
             foreach (Feed f in feedDelete)
             {
@@ -78,20 +78,20 @@ namespace BL.Controllers
         public void DeleteFeed(string url)
         {
             int indexDelete = -1;
-            for (int i = 0; i < feed.GetAll().Count; i++)
+            for (int i = 0; i < feedRepository.GetAll().Count; i++)
             {
-                if (string.Equals(feed.GetAll()[i].Url, url, StringComparison.OrdinalIgnoreCase))
+                if (string.Equals(feedRepository.GetAll()[i].Url, url, StringComparison.OrdinalIgnoreCase))
                     indexDelete = i;
             }
 
             if (indexDelete > -1)
-                feed.Delete(indexDelete);
+                feedRepository.Delete(indexDelete);
 
         }
 
         public List<Feed> GetAllExceptThisOne(string url)
         {
-            return (from Feed feed in feed.GetAll()
+            return (from Feed feed in feedRepository.GetAll()
                     where !string.Equals(feed.Url, url, StringComparison.OrdinalIgnoreCase)
                     select feed).ToList();
         }
@@ -102,12 +102,12 @@ namespace BL.Controllers
             Feed ny = GetFeed(url);
             DeleteFeed(url);
             SkapaFeedObjekt(ny.Namn, ny.Url, ny.UppdateringsTid, ny.Kategorier);
-            feed.SaveChanges();
+            feedRepository.SaveChanges();
         }
         public void BehovsFeedsUppdatera()
         {
             
-            foreach (Feed pod in feed.GetAll())
+            foreach (Feed pod in feedRepository.GetAll())
             {
                 
                 if (pod.BehovsUpdatera)
